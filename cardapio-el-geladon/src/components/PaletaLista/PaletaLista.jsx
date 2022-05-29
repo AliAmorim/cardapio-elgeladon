33283a22db1b75e1b7f2f02501ac8143355ce636
AliAmorim <alineamorimxd@hotmail.com>
2022-05-29 17:01:50 -0300

    commit

---
+++ cardapio-el-geladon/src/components/PaletaLista/PaletaLista.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import PaletaListaItem from 'components/PalestaListaItem/PaletaListaItem';
+import { paletas } from "../../mocks/paletas.js";
+import './PaletaLista.css';
+
+
+function PaletaLista() {
+  
+  const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+  const adicionarItem = (paletaIndex) => {
+          const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) +1 }
+          setPaletaSelecionada({ ...paletaSelecionada, ...paleta});
+  }
+
+  const removerItem = (paletaIndex) => {
+    const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) -1 }
+    setPaletaSelecionada({...paletaSelecionada, ...paleta});
+}
+
+
+
+  return <div className="PaletaLista">
+     {paletas.map((paleta, index) => (
+       <PaletaListaItem 
+       key ={ `PalestaListaItem-${index}`}
+       paleta={paleta}
+       quantidadeSelecionada={paletaSelecionada[index]}
+       index={index}
+       onRemove={index => removerItem(index)}
+       onAdd={index => adicionarItem(index)}/>
+    ))}
+  </div>;
+}
+
+export default PaletaLista;
